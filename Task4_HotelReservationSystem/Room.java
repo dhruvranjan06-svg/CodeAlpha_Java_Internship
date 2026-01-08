6cc2774a78f3e38c0669e09ef7bfbed09e02e4e7
dhruvranjan06-svg <dhruvranjan06@gmail.com>
2026-01-08 23:30:03 +0530

    Add Room class for hotel reservation system

---
+++ Task4_HotelReservationSystem/Room.java
@@ -0,0 +1,11 @@
+public class Room {
+    int roomNumber;
+    String category;
+    boolean isBooked;
+
+    Room(int roomNumber, String category) {
+        this.roomNumber = roomNumber;
+        this.category = category;
+        this.isBooked = false;
+    }
+}
